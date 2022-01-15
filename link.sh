f484ebc207c227c8b585f92f8c03c0d95baa8a74
Sean Bailey <hello@seanbailey.dev>
2022-01-15 13:12:25 +1100

    Configure git and more in `link.sh`

--- link.sh
+++ link.sh
@@ -1,17 +1,32 @@
 #!/bin/sh
 
 link() {
-  echo Linking $2 to $1
   rm -f $2 || true
   ln -s $(pwd)/$1 $2
 }
 
+echo "Configuring term..."
+link tmux.conf ~/.tmux.conf
+link starship.toml ~/.config/starship.toml
+
+echo "Configuring neovim..."
 link vimrc ~/.vimrc
 link vimrc ~/.config/nvim/init.vim
 link coc-settings.json ~/.config/nvim/coc-settings.json
-link tmux.conf ~/.tmux.conf
-link starship.toml ~/.config/starship.toml
+
+echo "Configuring ZSH..."
 link zsh/zshenv ~/.zshenv
 link zsh/zshrc ~/.zshrc
 
-echo Done! 🎉
+echo "Configuring doom emacs..."
+doom=${DOOMDIR=~/.doom.d}
+link doom/config.el $doom/config.el
+link doom/init.el $doom/init.el
+link doom/packages.el $doom/packages.el
+
+echo "Configuring git..."
+link .gitmessage ~/.gitmessage
+git config --global commit.template ~/.gitmessage
+git config --global core.editor emacs
+
+echo "Done! 🎉"
