f28e8cfe811ab732bedcf55cdda0243428456089
Sean Bailey <hello@seanbailey.dev>
2023-09-22 15:48:41 +1000

    Update dotfiles

--- link.sh
+++ link.sh
@@ -9,24 +9,13 @@ echo "Configuring term..."
 link tmux.conf ~/.tmux.conf
 link starship.toml ~/.config/starship.toml
 
-echo "Configuring neovim..."
-link vimrc ~/.vimrc
-link vimrc ~/.config/nvim/init.vim
-link coc-settings.json ~/.config/nvim/coc-settings.json
-
 echo "Configuring ZSH..."
 link zsh/zshenv ~/.zshenv
 link zsh/zshrc ~/.zshrc
 
-echo "Configuring doom emacs..."
-doom=${DOOMDIR=~/.doom.d}
-link doom/config.el $doom/config.el
-link doom/init.el $doom/init.el
-link doom/packages.el $doom/packages.el
-
 echo "Configuring git..."
 link .gitmessage ~/.gitmessage
 git config --global commit.template ~/.gitmessage
-git config --global core.editor emacs
+git config --global core.editor nvim
 
 echo "Done! 🎉"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,8 +4,7 @@ alias vim="nvim"
 
 alias rqp-auth="rqp auth -z secure -s kaos -r poweruser -e s.bailey@nib.com.au"
 
-# Git {{{
-# -------
+# Git
 alias ga="git add"
 alias gc="git commit"
 alias gd="git diff"
@@ -13,15 +12,8 @@ alias gl="git log"
 alias gp="git push"
 alias gs="git status"
 alias git-drop-merged="git branch --merged | egrep -v \"(^\*|master|main|dev)\" | xargs git branch -d"
-# }}}
 
-# NPM/Yarn {{{
-# ------------
-alias js="npm run"
-# }}}
-
-# exa (drop-in alternative for ls) {{{
-# ------------------------------------
+# exa (drop-in alternative for ls)
 if [ "$(command -v exa)" ]; then
   unalias -m 'll'
   unalias -m 'l'
@@ -30,17 +22,14 @@ if [ "$(command -v exa)" ]; then
   alias ls='exa -G --color auto --icons -a -s type'
   alias ll='exa -l --color always --icons -a -s type'
 fi
-# }}}
 
-# bat (drop-in alternative for cat) {{{
-# -------------------------------------
+# bat (drop-in alternative for cat)
 if [ "$(command -v bat)" ]; then
   unalias -m "cat"
   alias cat="bat --theme=OneHalfDark"
 fi
-# }}}
 
-# fd (drop-in alternative for find) {{{
-# -------------------------------------
-alias find="fd"
-# }}}
+# fd (drop-in alternative for find)
+if [ "$(command -v bat)" ]; then
+  alias find="fd"
+fi
